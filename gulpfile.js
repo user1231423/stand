4d07649f74e7200d27c154bda91c2f43c2c4cb39
João Gouveia <a13734@ebsaas.com>
2018-11-06 11:54:15 +0000

    Add remove unused css

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,28 @@
 var gulp = require('gulp'); 
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var purify = require('gulp-purifycss');
 
 //Detect when a change in files is made and sync with browser
 gulp.task('serve',['sass'],function(){
   browserSync.init({
-    server: "dist/"
+    server: "src/index.html"
   });
   gulp.watch("src/assets/scss/*.scss",['sass']);
-  gulp.watch("*.html").on('change',browserSync.reload);
+  gulp.watch("src/*.html").on('change',browserSync.reload);
 });
 
 //Compile scss into css
 gulp.task('sass', function () {
   return gulp.src('src/assets/scss/style.scss')
     .pipe(sass())
-    .pipe(csso())
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(browserSync.stream());
  });
+
+ 
+gulp.task('css', function() {
+  return gulp.src('dist/assets/css/style.css')
+    .pipe(purify(['src/*.html']))
+    .pipe(gulp.dest('dist/assets/css/'));
+});
