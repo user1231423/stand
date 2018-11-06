52b55990d51c54ca7e0d53cb3d20a773be632a3d
João Gouveia <a13734@ebsaas.com>
2018-11-06 12:47:49 +0000

    Folger structure update

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,24 @@ var purify = require('gulp-purifycss');
 //Detect when a change in .scss and .css is made and auto sync
 gulp.task('check-server',['sass'],function(){
   browserSync.init({
-    server: "src/index.html"
+    server: "src"
   });
-  gulp.watch("src/assets/scss/*.scss",['sass']);
-  gulp.watch("src/*.html").on('change',browserSync.reload);
-  gulp.watch("dist/assets/css/*.css",['css']);
+  gulp.watch("src/styles/scss/*.scss",['sass']);
   gulp.watch("src/*.html").on('change',browserSync.reload);
 });
 
 //Compile scss into css
 gulp.task('sass', function () {
-  return gulp.src('src/assets/scss/style.scss')
+  return gulp.src('src/styles/scss/style.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(gulp.dest('src/dist/assets/css'))
     .pipe(browserSync.stream());
  });
 
  //Remove all unused css
-gulp.task('css', function() {
+/* gulp.task('css', function() {
   return gulp.src('dist/assets/css/style.css')
-    .pipe(purify(['src/*.html']))
+    .pipe(purify(['src/a*.css']))
     .pipe(gulp.dest('dist/assets/css/'))
     .pipe(browserSync.stream());
-});
+}); */
