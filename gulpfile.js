c1308190a296011654cfdbe00b46f0c1715005fd
João Gouveia <a13734@ebsaas.com>
2018-11-06 12:18:06 +0000

    Add auto check and sript run

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var purify = require('gulp-purifycss');
 
-//Detect when a change in files is made and sync with browser
-gulp.task('serve',['sass'],function(){
+//Detect when a change in .scss and .css is made and auto sync
+gulp.task('check-server',['sass'],function(){
   browserSync.init({
     server: "src/index.html"
   });
   gulp.watch("src/assets/scss/*.scss",['sass']);
+  gulp.watch("src/*.html").on('change',browserSync.reload);
+  gulp.watch("dist/assets/css/*.css",['css']);
   gulp.watch("src/*.html").on('change',browserSync.reload);
 });
 
@@ -21,9 +23,10 @@ gulp.task('sass', function () {
     .pipe(browserSync.stream());
  });
 
- 
+ //Remove all unused css
 gulp.task('css', function() {
   return gulp.src('dist/assets/css/style.css')
     .pipe(purify(['src/*.html']))
-    .pipe(gulp.dest('dist/assets/css/'));
+    .pipe(gulp.dest('dist/assets/css/'))
+    .pipe(browserSync.stream());
 });
