597143d2bf7916e8ad7035c2f576cc6794914f40
João Gouveia <a13734@ebsaas.com>
2018-11-06 11:25:55 +0000

    change

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+//require all needed vars
 var gulp = require('gulp'); 
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+//Detect when a change in files is made and sync with browser
 gulp.task('serve',['sass'],function(){
   browserSync.init({
     server: "dist/"
@@ -10,6 +12,7 @@ gulp.task('serve',['sass'],function(){
   gulp.watch("*.html").on('change',browserSync.reload);
 });
 
+//Compile scss into css
 gulp.task('sass', function () {
   return gulp.src('src/assets/scss/style.scss')
     .pipe(sass())
