e32b3b1a46bb0dc9e9bcffdd39a3f20f94f66792
João Gouveia <a13734@ebsaas.com>
2018-11-06 11:24:01 +0000

    Add auto scss compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp'); 
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve',['sass'],function(){
+  browserSync.init({
+    server: "dist/"
+  });
+  gulp.watch("src/assets/scss/*.scss",['sass']);
+  gulp.watch("*.html").on('change',browserSync.reload);
+});
 
 gulp.task('sass', function () {
   return gulp.src('src/assets/scss/style.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/assets/css'));
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(browserSync.stream());
  });
