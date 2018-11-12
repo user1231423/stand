47e75faf034dcf5a76acb4c3c022b79722880b33
João Gouveia <a13734@ebsaas.com>
2018-11-12 14:04:59 +0000

    Gulp file changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,31 @@ var browserSync = require('browser-sync').create();
 var purify = require('gulp-purifycss');
 
 //Detect when a change in .scss and .css is made and auto sync
-gulp.task('check-server',['sass'],function(){
+gulp.task('check-server',function(){
   browserSync.init({
     server: "dist"
   });
   gulp.watch("src/assets/scss/*.scss",['sass']);
+  gulp.watch("src/assets/scss/*.scss").on('change',browserSync.reload);
   gulp.watch("src/*.html",['move-html']);
   gulp.watch("src/*.html").on('change',browserSync.reload);
 });
 
 //Compile scss into css
 gulp.task('sass', function () {
-  return gulp.src('src/assets/scss/style.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('src/dist/assets/css'))
-    .pipe(browserSync.stream());
+    gulp.src('src/assets/scss/style.scss')
+    .pipe(gulp.dest('dist/assets/css'))
  });
 
  //Get Font Awesome fonts
  gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/assets/fonts'))
+    gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest('dist/assets/fonts/'))
 })
 
 gulp.task('move-html', function() {
-  gulp.src("src/*.html")
-      .pipe(gulp.dest('dist/'));
+    gulp.src("src/*.html")
+    .pipe(gulp.dest('dist/'));
 });
 
  //Remove all unused css
