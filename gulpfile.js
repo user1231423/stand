a19a35920491af6cbd3ac3aa694fc4976a2a8441
João Gouveia <a13734@ebsaas.com>
2018-11-12 14:07:13 +0000

    Gulp file changes

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,29 @@ var browserSync = require('browser-sync').create();
 var purify = require('gulp-purifycss');
 
 //Detect when a change in .scss and .css is made and auto sync
-gulp.task('check-server',function(){
+gulp.task('check-server',['sass'],function(){
   browserSync.init({
     server: "dist"
   });
-  gulp.watch("src/assets/scss/*.scss",['sass']);
+  gulp.watch("src/assets/scss/*.scss",['compile-sass']);
   gulp.watch("src/assets/scss/*.scss").on('change',browserSync.reload);
   gulp.watch("src/*.html",['move-html']);
   gulp.watch("src/*.html").on('change',browserSync.reload);
 });
 
 //Compile scss into css
-gulp.task('sass', function () {
+gulp.task('compile-sass', function () {
     gulp.src('src/assets/scss/style.scss')
     .pipe(gulp.dest('dist/assets/css'))
  });
 
  //Get Font Awesome fonts
- gulp.task('fonts', function() {
+ gulp.task('move-fonts', function() {
     gulp.src('node_modules/font-awesome/fonts/*')
     .pipe(gulp.dest('dist/assets/fonts/'))
 })
 
+//move html files to dist
 gulp.task('move-html', function() {
     gulp.src("src/*.html")
     .pipe(gulp.dest('dist/'));
